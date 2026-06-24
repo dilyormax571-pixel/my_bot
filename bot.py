8156a00aa1c0d1b851378e84d8f15c61464266e2
dilyormax571-pixel <dilyormax571@gmail.com>
2026-06-24 19:17:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+import telebot
 
+TOKEN = "BU_YERGA_BOT_TOKENINGNI_YOZ"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Salom! Bot ishlayapti! 🤖")
+
+print("Bot ishga tushdi...")
+bot.infinity_polling()
